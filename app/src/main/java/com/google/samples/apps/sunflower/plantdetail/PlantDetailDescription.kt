cbdb6cee0bfc8f94bd077ff79b0a8f3042ec6d1e
TiliaSNogueira <tiliasalomao@gmail.com>
2023-07-25 17:07:30 -0300

    CompositionStrategy, dark mode preview and linking Compose theme with sml theme by using themeadapter

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,7 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.content.res.Configuration
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.compose.foundation.layout.Column
@@ -39,6 +40,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
 import androidx.core.widget.TextViewCompat
+import com.google.accompanist.themeadapter.material.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -138,15 +140,16 @@ fun PlantName(name: String) {
 @Preview
 @Composable
 private fun PlantDescriptionPreview() {
-    MaterialTheme {
+    MdcTheme {
         PlantDescription("HTML<br><br>description")
     }
 }
 
+@Preview(uiMode =Configuration.UI_MODE_NIGHT_YES )
 @Preview
 @Composable
 fun PlantDetailDescriptionPreview() {
-    MaterialTheme {
+    MdcTheme {
         val plant = Plant("id", "Apple", "description", 3, 30, "")
         PlantDetailContent(plant = plant)
     }
@@ -155,7 +158,7 @@ fun PlantDetailDescriptionPreview() {
 @Preview
 @Composable
 fun PlantNamePreview() {
-    MaterialTheme {
+    MdcTheme {
         PlantName(name = "Tília")
     }
 }
