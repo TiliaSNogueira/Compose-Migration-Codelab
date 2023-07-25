a0bebd0046af2f4263f4ade9b1152cb1db3d89d0
TiliaSNogueira <tiliasalomao@gmail.com>
2023-07-25 16:52:38 -0300

    Creating AndroidView inside Composable

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,8 +16,11 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
@@ -25,6 +28,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -32,6 +36,9 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+import androidx.core.widget.TextViewCompat
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -57,8 +64,34 @@ fun PlantDetailContent(plant: Plant) {
     ) {
         PlantName(name = plant.name)
         PlantWatering(wateringInterval = plant.wateringInterval)
-        //PlantDescription
+        PlantDescription(description = plant.description)
     }
+}
+
+@Composable
+fun PlantDescription(description: String) {
+
+    // Remembers the HTML formatted description. Re-executes on a new description
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+                TextViewCompat.setTextAppearance(this, android.R.style.TextAppearance_Medium)
+            }
+        },
+        update = { textView ->
+          //  textView.text = HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+            textView.text = htmlDescription
+        },
+        modifier = Modifier
+            .padding(top = dimensionResource(id = R.dimen.margin_small))
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .heightIn(min = dimensionResource(id = R.dimen.plant_description_min_height))
+    )
 }
 
 @Composable
@@ -100,6 +133,14 @@ fun PlantName(name: String) {
             .wrapContentWidth(Alignment.CenterHorizontally),
         style = MaterialTheme.typography.h5
     )
+}
+
+@Preview
+@Composable
+private fun PlantDescriptionPreview() {
+    MaterialTheme {
+        PlantDescription("HTML<br><br>description")
+    }
 }
 
 @Preview
